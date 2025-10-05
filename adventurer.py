fd24c4d2a60870efa99b8bf66e998a5daffaeda3
Madina-Abdusamatova <madinauniv@gmail.com>
2025-10-06 00:01:43 +0500

    Initial commit

---
+++ adventurer.py
@@ -0,0 +1,19 @@
+name=input("What is your name? ")
+class1= input("What class are you? ")
+origin=input("Where are you from? ")
+mission=input("What are you here to retrieve for? ")
+nemesis=input("What is your nemesis's name? ")
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print( "KINGDOM OF CODERA - ADVENTURER'S PASSPORT")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("Name: ", name)
+print("Class: ", class1)
+print("Origin: ", origin)
+
+print("Known Alias: The",origin, class1 )
+
+print("------------------------------------------")
+
+print("Current Mission: ")
+print("Retrieve the", mission,"from the clutches of", nemesis,"!")
